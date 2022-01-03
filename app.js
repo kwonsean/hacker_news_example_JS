b6c22e0cfa28f91faae06d45e38d8179ada0137c
kwonsean <gownn12@naver.com>
2022-01-03 19:56:09 +0900

    feat: page기능 구현
    - 1~3페이지 구현 (10개 데이터씩)
    - 이 이상, 이하 페이지 이동 방어

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const root = document.getElementById('root')
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 
+const store = {
+  currentPage: 1,
+}
+
 // ajax 호출을 함수로 묶어 중복 제거
 function getData(url) {
   ajax.open('GET', url, false)
@@ -24,10 +28,10 @@ function newsFeed() {
 
   newsList.push('<ul>')
 
-  for (let i = 0; i < newsFeed.length; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
   <li>
-    <a href="#${newsFeed[i].id}">
+    <a href="#/show/${newsFeed[i].id}">
       ${newsFeed[i].title} (${newsFeed[i].comments_count})
     </a>
   </li>
@@ -35,20 +39,29 @@ function newsFeed() {
   }
 
   newsList.push('</ul>')
-
+  newsList.push(`
+    <div>
+      <a href='#/page/${
+        store.currentPage > 1 ? store.currentPage - 1 : 1
+      }'>이전 페이지</a>
+      <a href='#/page/${
+        store.currentPage < 3 ? store.currentPage + 1 : 3
+      }'>다음 페이지</a>
+    </div>
+  `)
   root.innerHTML = newsList.join('')
 }
 
 function newsDetail() {
-  const id = location.hash.slice(1)
+  const id = location.hash.slice(7)
   const newsContent = getData(CONTENT_URL.replace('@id', id)) // !! replace 활용능력
-  // console.log(newsContent)
+  // console.log('newsContent!', newsContent)
 
   const title = document.createElement('h1')
   root.innerHTML = `
   <h1>${newsContent.title}</h1>
   <div>
-    <a href='#'>목록으로</a>
+    <a href='#/page/${store.currentPage}'>목록으로</a>
   </div>
   `
 }
@@ -56,6 +69,9 @@ function newsDetail() {
 function router() {
   const routePath = location.hash // !! location.hash에 #만 들어 있는 경우에는 빈값이 반환된다.
   if (routePath === '') {
+    newsFeed()
+  } else if (routePath.indexOf('#/page/') >= 0) {
+    store.currentPage = Number(routePath.substring(7))
     newsFeed()
   } else {
     newsDetail()
